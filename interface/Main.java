8e6f21f48de4df1c13815d06a029c1eeaed3af18
silla2807 <79395251+silla2807@users.noreply.github.com>
2023-07-13 11:47:29 +0530

    Create Main.java

---
+++ interface/Main.java
@@ -0,0 +1,13 @@
+class Main
+{
+  public static void main (String[]args)
+  {
+    double length = 9.0;
+    double breadth = 8.0;
+    Rectangle r = new Rectangle (length, breadth);
+
+
+      r.getArea ();
+
+  }
+}
